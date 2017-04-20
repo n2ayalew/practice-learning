52fa751ec830701c130bba331f276ee6c55bd4e2
Nathaniel Ayalew <nayalew@blackberry.com>
2017-04-20 11:20:34 -0400

    Create functions for reading text data into matrix and normalizing for kNN

--- kNN/kNN.py
+++ kNN/kNN.py
@@ -1,9 +1,10 @@
 from numpy import *
+import re
 import operator
 import bisect
 import math
 
-def build_data_set():
+def build_data_set0():
 	groups = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
 	labels = ['A', 'A', 'B', 'B']
 	return groups, labels
@@ -32,9 +33,42 @@ def kNN_classify0(point, data_set, labels, k):
 	else:
 		print 'CLASS: B'
 
-def kNN_classify(point, data_set, labels, k):
+def kNN_classify1(point, data_set, labels, k):
 	num_points = shape(data_set)[0]
 	point_tile = tile(point, (num_points, 1))
+	diffs = point_tile - data_set
+	squares = diffs**2
+	sums = sum(squares, 1)
+	dists = sums**0.5
+	sorted_indicies = argsort(dists)
+        class_score = {}
+        for i in range(k):
+            label = labels[sorted_indicies[i]]
+            class_score[label] = class_score.get(label, 0) + 1
+        sorted_classes = sorted(class_score.items(), key=operator.itemgetter(1))
+        return sorted_classes[0][0]
 
-g,l = build_data_set()
-kNN_classify([0.2, 0.3], g, l, 3)
+def file_to_mat(filename):
+    lines = open(filename, 'r')
+    data_set = []
+    labels = []
+    for line in lines:
+        line = line.strip()
+        lst = line.split('\t')
+        data_set.append(lst[:-1])
+        labels.append(lst[-1])
+    return array(data_set).astype(float), labels
+
+def auto_norm(data):
+    num_columns = data.shape[1]
+    maxs = data.max(0)
+    mins = data.min(0)
+    ranges = maxs - mins
+    num_rows = data.shape[0]
+    for i in range(num_rows):
+            data[i] = (data[i] - mins) / (ranges)
+    return data
+
+ds, cs = file_to_mat('datingTestSet.txt')
+print ds
+print auto_norm(ds)
